4ca30107bd026878b162b632d24febcfdb9881ba
victory-sokolov <viktorsokolov.and@gmail.com>
2020-09-19 13:17:50 +0300

    update tempalte folder

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ import os
 from pathlib import Path
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+
 from app.config import config
 
 db = SQLAlchemy()
@@ -13,9 +13,7 @@ bcrypt = Bcrypt()
 
 
 def create_app(config_name) -> object:
-    app = Flask(__name__,
-                static_folder="../../client",
-                template_folder="../../client")
+    app = Flask(__name__, template_folder="templates")
 
     if not isinstance(config_name, str):
         config_name = os.getenv('FLASK_CONFIG', 'default')
@@ -29,8 +27,8 @@ def create_app(config_name) -> object:
     db.init_app(app)
     bcrypt.init_app(app)
 
-
     # shell context for flask cli
+
     @app.shell_context_processor
     def ctx():
         return {'app': app, 'db': db}
