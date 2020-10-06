6e0f62c64ed170ed192535a385136fedc87add6b
victory-sokolov <viktorsokolov.and@gmail.com>
2020-10-06 21:34:33 +0300

    Recognition class

--- app/Recognition.py
+++ app/Recognition.py
@@ -1,22 +1,36 @@
-import easyocr
+import cv2
+import numpy as np
+import pytesseract
 
-class Recognition:
-
-    def easyocr_recognition(self, filename: str):
-        reader = easyocr.Reader(['lv'], gpu=False)
-        result = reader.readtext(filename, detail=0)
-
-        return list(
-            filter(None, result.split("\n"))
-        )
+# from redis import Redis
+# from rq import Queue
 
 
+class Recogniser(object):
 
+    def __init__(self, lang: str, image):
+        self._lang = lang
+        self.image = image
 
-image = "IMG_20200123_175929.jpg"
+    def recognise(self) -> str:
+        # img_cv = cv2.imread(r'example.jpg')
+        img_rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        blur = cv2.GaussianBlur(img_rgb, (5, 5), 0)
+        thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)[1]
+        # cv2.imwrite("output.jpg", thresh)
+        result = pytesseract.image_to_osd(thresh, lang=self._lang)
+        print(result)
+        return result
 
-recognition = Recognition()
+    # def recognision_queue(self, image):
+    #     image_process = ImageProcessing(image)
 
-result = recognition.easyocr_recognition(image)
+    #     q = Queue(connection=Redis())
 
-print(result)
+    #     pipeline_job = q.enqueue(image_process.run_pipeline)
+    #     q.enqueue(self.recognise, args=(
+    #         image), depends_on=pipeline_job)
+
+    #     print(
+    #         f"Task {pipeline_job.id} added to queue at {pipeline_job.enqueued_at}. {len(q)} tasks in the queue"
+    #     )
