09ff7df90dfb143b3aaa63f3fcde77e2467d716d
victory-sokolov <viktorsokolov.and@gmail.com>
2020-10-07 19:50:48 +0300

    check if file is archive

--- app/utils/file.py
+++ app/utils/file.py
@@ -4,8 +4,19 @@ import shutil
 from fastapi import File
 
 
-def save_file(file: File):
+def save_file(file: File) -> None:
     file_object = file.file
     upload_folder = open(os.path.join("./static", file.filename), 'wb+')
     shutil.copyfileobj(file_object, upload_folder)
     upload_folder.close()
+
+
+def is_archive_file(file):
+    ext = os.path.splitext(file)[1]
+    archive_formats = shutil.get_archive_formats()
+    supported_format = [x[0] for x in archive_formats]
+
+    if ext[1:] in supported_format:
+        return True
+
+    return False
