6b1614f8840f4d1994eb69cc9f186dab286b88d3
victory-sokolov <viktorsokolov.and@gmail.com>
2020-10-08 14:38:20 +0300

    unarchive files and detect mrz

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
-import os
+import shutil
+from os import getcwd, listdir
+from os.path import isfile, join
 
 from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from config import config
 from detect_mrz import get_mrz
 from Recognition import Recogniser
-from utils.file import save_file
+from utils.file import is_archive_file, save_file
 
-config_name = os.getenv('FLASK_CONFIG', 'default')
+config_name = config['development']
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
@@ -21,10 +24,23 @@ def upload_form(request: Request):
 
 @app.post('/upload')
 def upload_image(file: UploadFile = File(...)):
-    save_file(file)
-    mrz = get_mrz(file.filename)
+    f = file.filename
 
-    recogniser = Recogniser('eng', mrz)
-    data = recogniser.recognise()
+    if f.endswith(tuple(config_name.ALLOWED_IMAGE_EXTENSIONS)):
+        save_file(file)
+        if is_archive_file(f):
+            folder = f"static/{f.split('.')[0]}"
+            shutil.unpack_archive(f'{getcwd()}/static/{f}', folder)
+            files = [f for f in listdir(folder) if isfile(join(folder, f))]
 
-    return {'data': data}, 200
+            mrz = get_mrz(files)
+            # recogniser = Recogniser('eng', mrz)
+            # data = recogniser.recognise()
+            return {'Status': 'Ok'}, 200
+
+        mrz = get_mrz(f)
+        recogniser = Recogniser('eng', mrz)
+        data = recogniser.recognise()
+        return {'data': data}, 200
+
+    return {'Status': 'File not allowed'}
