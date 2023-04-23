af3518440e54201f9c99c87a88e9f05eed1574d7
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-04-23 20:11:25 +0300

    update

--- app/app.py
+++ app/app.py
@@ -1,18 +1,17 @@
+import shutil
 from os import getcwd, listdir
 from os.path import isfile, join
-import shutil
-from config import config
-from containers import RecognitionContainer
+
 from fastapi import FastAPI, File, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from utils.helpers import clean
-from utils.file import is_archive_file, save_file
-
 from loguru import logger
 
+from app.config import config
+from app.containers import RecognitionContainer
+from app.utils.file import is_archive_file, save_file
+from app.utils.helpers import clean
 
 config_name = config["development"]
 app = FastAPI(debug=True)
@@ -30,39 +29,38 @@ def upload_form(request: Request):
 def ocr_result(request: Request):
     nums = {"image": "static/IDCardDataSet/document_1.jpg"}
     return templates.TemplateResponse(
-        "result.html", {"request": request, "results": nums}
+        "result.html",
+        {"request": request, "results": nums},
     )
 
 
 @app.post("/upload", response_class=HTMLResponse)
 def upload_image(request: Request, file: UploadFile = File(...)):
-    f = file.filename
-    logger.info(f"Uploading {f}")
-    extension = f.split(".")[-1]
+    f_name = file.filename
+    logger.info(f"Uploading {f_name}")
+    extension = f_name.split(".")[-1]
     processor = RecognitionContainer.processor()
 
-    if extension in config_name.ALLOWED_IMAGE_EXTENSIONS:
-        save_file(file)
-        if is_archive_file(extension):
-            logger.info("Unarchiving files...")
-            folder = f"{f.split('.')[0]}/"
-            # file path where to extract archive images
-            file_path = f"static/{folder}"
-            shutil.unpack_archive(f"{getcwd()}/static/images/{f}", file_path)
-            files = [
-                folder + f for f in listdir(file_path) if isfile(join(file_path, f))
-            ]
+    if extension not in config_name.ALLOWED_IMAGE_EXTENSIONS:
+        return {"status": f"File with extension {extension} is not allowed"}
 
-            ocr = processor.process(files)
-            ocr = clean(ocr)
-            return templates.TemplateResponse(
-                "result.html", {"request": request, "data": ocr}
-            )
-
-        ocr = processor.process(f)
+    save_file(file)
+    if not is_archive_file(extension):
+        ocr = processor.process(f_name)
         # ocr = clean(ocr)
         return templates.TemplateResponse(
-            "result.html", {"request": request, "data": ocr}
+            "result.html",
+            {"request": request, "data": ocr},
         )
 
-    return {"status": f"File with extension {extension} is not allowed"}
+    # Handling archived files
+    logger.info("Archiving files...")
+    folder = f"{f_name.split('.')[0]}/"
+    # file path where to extract archive images
+    file_path = f"static/{folder}"
+    shutil.unpack_archive(f"{getcwd()}/static/images/{f_name}", file_path)
+    files = [folder + f for f in listdir(file_path) if isfile(join(file_path, f))]
+
+    ocr = processor.process(files)
+    ocr = clean(ocr)
+    return templates.TemplateResponse("result.html", {"request": request, "data": ocr})
