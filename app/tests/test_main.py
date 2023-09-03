695a0d2ecfcecbff754d956011c33ec4f04c4ae5
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-09-03 14:58:01 +0300

    Update test file

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,6 +1,9 @@
 import unittest
 from io import BytesIO
 
+import cv2
+import numpy as np
+
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -12,14 +15,27 @@ class MainTestCase(unittest.TestCase):
         cls.client = TestClient(app)
 
     def test_upload_image(self):
-        # create a mock image
-        mock_image = BytesIO()
-        mock_image.write(b"mock image data")
-        mock_image.seek(0)
+        # Create a blank 2x2 pixel image with 3 color channels (BGR)
+        image = np.zeros((2, 2, 3), dtype=np.uint8)
+
+        # Set pixel colors (red and blue)
+        image[0, 0] = [0, 0, 255]  # Red pixel at (0, 0)
+        image[1, 1] = [255, 0, 0]  # Blue pixel at (1, 1)
+        # Convert the image to the RGB format (if needed)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Create a BytesIO object to save the image
+        image_bytesio = BytesIO()
+
+        # Use OpenCV's imencode function to encode the image and write it to the BytesIO object
+        retval, buffer = cv2.imencode(".png", image_rgb)
+        if retval:
+            image_bytesio.write(buffer.tobytes())
+            image_bytesio.seek(0)  # Reset the BytesIO position to the beginning
 
         # pass the mock image to the `files` parameter
         response = self.client.post(
-            "/upload", files={"file": ("mock.png", mock_image, "image/png")},
+            "/upload",
+            files={"file": ("mock.png", image_bytesio, "image/png")},
         )
 
         self.assertEqual(response.status_code, 200)
