c8f24f2ed4b1f3c5478738616ff39e0aa70d69f5
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-05-03 15:28:06 +0300

    add healthcheck endpoint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .extract.views import router as extract_views
+from .health import health
 
 router = APIRouter()
 router.include_router(router=extract_views, prefix="/ocr")
+router.include_router(router=health)
