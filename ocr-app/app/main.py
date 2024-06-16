bdca04dce07ea13746cf727b4810235987755c66
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-06-16 09:24:19 +0300

    update host

--- ocr-app/app/main.py
+++ ocr-app/app/main.py
@@ -4,7 +4,7 @@ from core.config import config
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8082,
         reload=config.DEBUG,
         ssl_keyfile="./certs/key.pem",
