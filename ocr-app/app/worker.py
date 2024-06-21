c18fb5e4ea7c0385ed2025d97cc644abdc87741c
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-06-21 18:43:16 +0300

    add Celery config

---
+++ ocr-app/app/worker.py
@@ -0,0 +1,11 @@
+import os
+
+from celery import Celery
+
+app = Celery("ocrapp")
+
+
+app.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6389")
+app.conf.result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND", "redis://localhost:6389"
+)
