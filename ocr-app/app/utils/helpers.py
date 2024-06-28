ccb81d58f94ee76ba910544fa98e0d6590feba29
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-06-28 22:59:55 +0300

    add is_base64 util

--- ocr-app/app/utils/helpers.py
+++ ocr-app/app/utils/helpers.py
@@ -1,5 +1,20 @@
+import base64
 import time
 from functools import wraps
+
+
+def is_base64(sb: str) -> bool:
+    try:
+        if isinstance(sb, str):
+            # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, "ascii")
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        else:
+            raise ValueError("Argument must be string or bytes")
+        return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
 
 
 def clean(txt_data):
