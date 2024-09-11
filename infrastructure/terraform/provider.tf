f1321f0500a5e9225ef8971fc7c733ec16439fac
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-09-11 19:46:36 +0300

    update provider

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
